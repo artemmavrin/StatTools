607a627101e7e9f2ede55868876974a16a6c33be
Artem Mavrin <amavrin@ucsd.edu>
2018-01-23 21:20:29 -0800

    Updated qqplot implementation

--- mltools/visualization/qqplot.py
+++ mltools/visualization/qqplot.py
@@ -5,48 +5,53 @@ import scipy.stats as st
 import matplotlib.pyplot as plt
 
 
-def qqplot(x, inv_cdf=None, inv_cdf_kwargs=None, ax=None, ax_kwargs=None, *args,
+def qqplot(y, qfunc=None, qfunc_kwargs=None, ax=None, ax_kwargs=None, *args,
            **kwargs):
-    """Draw a QQ plot for a data sample.
+    """Draw a QQ plot comparing a data sample to a theoretical distribution.
 
     Parameters
     ----------
-    x: array-like
-        The data
-    inv_cdf: callable, optional
-        The inverse cumulative distribution function of the theoretical
-        distribution the data is being compared to.
-        If this is not specified, the normal distribution will be used.
-    inv_cdf_kwargs: dict, optional
-        Additional keyword arguments for the inverse cumulative distribution
-        function.
-    ax: matplotlib axis
+    y: array-like
+        The data sample.
+    qfunc: callable, optional
+        The quantile function ("inverse" cumulative distribution function) of
+        the theoretical distribution the data is being compared to.
+        If this is not specified, the normal distribution quantile function will
+        be used.
+    qfunc_kwargs: dict, optional
+        Additional keyword arguments for the quantile function.
+    ax: matplotlib axis, optional
         The axis on which to draw the QQ plot.
         If this is not specified, the current axis will be used.
     ax_kwargs: dict, optional
-        Additional keyword arguments to pass to matplotlib.pyplot.gca()
+        Additional keyword arguments to pass to matplotlib.pyplot.gca() if
+        axis is specified.
     args: sequence
         Additional positional arguments to pass to matplotlib.pyplot.scatter().
     kwargs: dict
         Additional keyword arguments to pass to matplotlib.pyplot.scatter().
     """
-    x = np.sort(x)
-    n = x.size
+    # Order statistics of the data sample
+    y = np.sort(y)
+    n = y.size
 
-    if inv_cdf is None:
+    # Percentiles for the theoretical distribution
+    p = (np.arange(n) + 0.5) / n
+
+    if qfunc is None:
         # Normal QQ plot by default
-        y = st.norm.ppf((np.arange(n) + 0.5) / n, loc=x.mean(), scale=x.std())
+        x = st.norm.ppf(p, loc=y.mean(), scale=y.std())
     else:
-        if inv_cdf_kwargs is None:
-            inv_cdf_kwargs = {}
-        y = inv_cdf((np.arange(n) + 0.5) / n, **inv_cdf_kwargs)
+        if qfunc_kwargs is None:
+            qfunc_kwargs = {}
+        x = qfunc(p, **qfunc_kwargs)
 
     if "c" not in kwargs:
         # Default color represents the distance to the diagonal line
-        kwargs["c"] = np.sqrt((x - (x + y) / 2) ** 2 + (y - (x + y) / 2) ** 2)
+        kwargs["c"] = np.abs(x - y)
     if "cmap" not in kwargs:
         # Default color map
-        kwargs["cmap"] = plt.cm.coolwarm
+        kwargs["cmap"] = plt.get_cmap("coolwarm")
 
     if ax is None:
         if ax_kwargs is None:
@@ -54,11 +59,14 @@ def qqplot(x, inv_cdf=None, inv_cdf_kwargs=None, ax=None, ax_kwargs=None, *args,
         ax = plt.gca(**ax_kwargs)
 
     ax.scatter(x, y, zorder=2, *args, **kwargs)
+    ax.set_xlabel("Theoretical quantiles")
+    ax.set_ylabel("Observed quantiles")
 
-    # diagonal line for comparison
+    # Diagonal line for comparison
     x_min, x_max = ax.get_xlim()
     y_min, y_max = ax.get_ylim()
-    a = (min(x_min, y_min), max(x_max, y_max))
-    ax.plot(a, a, c="black", zorder=1)
-    ax.set_xlim(a)
-    ax.set_ylim(a)
+    bounds = (min(x_min, y_min), max(x_max, y_max))
+    ax.plot(bounds, bounds, c="black", zorder=1)
+    ax.set_xlim(bounds)
+    ax.set_ylim(bounds)
+    ax.set_aspect("equal")
