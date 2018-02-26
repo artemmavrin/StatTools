df80c7af2bc4708863f3347b5138c0073b08bb5a
Artem Mavrin <amavrin@ucsd.edu>
2018-02-26 09:58:22 -0800

    Implemented ridge and LASSO linear regression

--- mltools/glm/linear_regression.py
+++ mltools/glm/linear_regression.py
@@ -4,6 +4,45 @@ import numpy as np
 
 from .generalized_linear_model import GeneralizedLinearModel
 from ..generic import Regressor
+from ..optimization import Optimizer
+from ..regularization import lasso, ridge
+
+
+class MSELoss(object):
+    """Mean squared error loss function for linear regression."""
+
+    def __init__(self, x, y):
+        """Initialize with the training data.
+
+        Parameters
+        ----------
+        x : array-like
+            Training data feature matrix.
+        y : array-like
+            Training data target vector.
+        """
+        self.x = np.asarray(x)
+        self.y = np.asarray(y)
+        self.n_samples = self.x.shape[0]
+
+        if self.n_samples != self.y.shape[0]:
+            raise ValueError(
+                f"Unequal number of samples: "
+                f"{self.n_samples} feature samples, "
+                f"{self.y.shape[0]} target samples")
+
+    def __call__(self, w):
+        """Compute the mean squared error loss for the training data."""
+        residuals = self.x.dot(w) - self.y
+        return residuals.dot(residuals) / self.n_samples
+
+    def grad(self, w):
+        """Compute the gradient of the mean squared error loss."""
+        return 2 * self.x.T.dot(self.x.dot(w) - self.y) / self.n_samples
+
+    def hess(self, _):
+        """Compute the Hessian of the mean squared error loss."""
+        return 2 * self.x.T.dot(self.x) / self.n_samples
 
 
 class LinearRegression(GeneralizedLinearModel, Regressor):
@@ -13,17 +52,35 @@ class LinearRegression(GeneralizedLinearModel, Regressor):
     # of course its own inverse).
     _inv_link = staticmethod(lambda x: x)
 
-    def __init__(self, intercept=True):
+    def __init__(self, penalty=None, lam=0.1, intercept=True, mle=False):
         """Initialize a LinearRegression object.
 
         Parameters
         ----------
-        intercept: bool
+        penalty: str, optional
+            Type of regularization to impose (none if None).
+            Currently supported:
+            None
+                No regularization
+            "l1"
+                L1 regularization (AKA LASSO regression)
+            "l2"
+                L2 regularization (AKA ridge regression)
+        lam: float, optional
+            Regularization parameter.
+        intercept: bool, optional
             Indicates whether the module should fit an intercept term.
+        mle: bool, optional
+            Indicate whether to fit using maximum likelihood estimation instead
+            of least squares if `penalty` is None. If `penalty` is not None,
+            this parameter is ignored.
         """
+        self.penalty = penalty
+        self.lam = lam
         self.intercept = intercept
+        self.mle = mle
 
-    def fit(self, x, y):
+    def fit(self, x, y, optimizer=None, *args, **kwargs):
         """Train the linear regression model.
 
         Parameters
@@ -32,6 +89,14 @@ class LinearRegression(GeneralizedLinearModel, Regressor):
             Feature matrix
         y: array-like
             Target vector
+        optimizer: Optimizer
+            Specifies the optimization algorithm used.
+        args: sequence
+            Additional positional arguments to pass to the optimizer's
+            `optimize` method.
+        kwargs: dict
+            Additional keyword arguments to pass to the optimizer's `optimize`
+            method.
 
         Returns
         -------
@@ -39,7 +104,29 @@ class LinearRegression(GeneralizedLinearModel, Regressor):
         """
         x = self._preprocess_features(x, fitting=True)
         y = self._preprocess_target(y)
-        self._weights, *_ = np.linalg.lstsq(x, y, rcond=None)
+
+        if self.penalty is None and not self.mle:
+            # Least squares regression
+            self._weights, *_ = np.linalg.lstsq(x, y, rcond=None)
+        else:
+            # Maximum likelihood estimation
+            self.loss = MSELoss(x, y)
+
+            if self.penalty == "l1":
+                self.loss = lasso(self.lam, self.loss)
+            elif self.penalty == "l2":
+                self.loss = ridge(self.lam, self.loss)
+            elif self.penalty is not None:
+                raise ValueError(f"Unknown penalty type: {self.penalty}")
+
+            if not isinstance(optimizer, Optimizer):
+                raise ValueError(f"Unknown minimization method: {optimizer}")
+
+            w0 = np.zeros(x.shape[1])
+
+            self._weights = optimizer.optimize(x0=w0, func=self.loss,
+                                               *args, **kwargs)
+
         self._fitted = True
         return self
 
--- mltools/glm/test/test_linear_regression.py
+++ mltools/glm/test/test_linear_regression.py
@@ -6,6 +6,7 @@ import unittest
 import numpy as np
 
 from mltools.glm import LinearRegression
+from mltools.optimization import GradientDescent, NewtonRaphson
 
 
 class TestLinearRegression(unittest.TestCase):
@@ -15,6 +16,15 @@ class TestLinearRegression(unittest.TestCase):
         y = [1, 2, 1.3, 3.75, 2.25]
         lr = LinearRegression()
         lr.fit(x, y)
+        intercept, slope = lr._weights
+        self.assertAlmostEqual(intercept, 0.785)
+        self.assertAlmostEqual(slope, 0.425)
+
+    def test_easy_1d_MLE(self):
+        x = [1, 2, 3, 4, 5]
+        y = [1, 2, 1.3, 3.75, 2.25]
+        lr = LinearRegression(mle=True)
+        lr.fit(x, y, optimizer=NewtonRaphson(iterations=100))
         intercept, slope = lr._weights
         self.assertAlmostEqual(intercept, 0.785)
         self.assertAlmostEqual(slope, 0.425)
@@ -35,6 +45,18 @@ class TestLinearRegression(unittest.TestCase):
             theta, phi = lr._weights
             self.assertAlmostEqual(theta, (y1 + 2 * y2 + y3) / 6)
             self.assertAlmostEqual(phi, (2 * y3 - y2) / 5)
+
+    def test_easy_2d_MLE(self):
+        x = [[1, 0], [2, -1], [1, 2]]
+        ys = np.linspace(-5, 5, num=10)
+        for y1, y2, y3 in itertools.product(ys, ys, ys):
+            y = [y1, y2, y3]
+            lr = LinearRegression(intercept=False, mle=True)
+            lr.fit(x, y, optimizer=GradientDescent(iterations=500))
+            theta, phi = lr._weights
+            self.assertAlmostEqual(theta, (y1 + 2 * y2 + y3) / 6)
+            self.assertAlmostEqual(phi, (2 * y3 - y2) / 5)
+
 
 
 if __name__ == "__main__":
