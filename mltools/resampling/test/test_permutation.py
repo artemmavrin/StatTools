4912cbcb2431d484dece0f8663853098c2692544
Artem Mavrin <amavrin@ucsd.edu>
2018-03-05 00:59:14 -0800

    Added `lady tasting tea' to permutation test unit tests

--- mltools/resampling/test/test_permutation.py
+++ mltools/resampling/test/test_permutation.py
@@ -8,6 +8,26 @@ from mltools.resampling import PermutationTest
 
 
 class TestPermutationTest(unittest.TestCase):
+    def test_lady_drinking_tea(self):
+        """Fisher's lady drinking tea exact permutation test. Wikipedia:
+         https://en.wikipedia.org/wiki/Lady_tasting_tea
+        """
+        # Let 0 = milk first, 1 = tea first
+
+        # Bristol's correct guesses about cups prepared by adding milk first
+        milk = [0, 0, 0, 0]
+
+        # Bristol's correct guesses about cups prepared by adding tea first
+        tea = [1, 1, 1, 1]
+
+        # Test statistic: number of correct guesses
+        def stat(m, t):
+            return np.sum(np.equal(m, 0)) + np.sum(np.equal(t, 1))
+
+        pt = PermutationTest(milk, tea, stat=stat)
+
+        self.assertAlmostEqual(pt.p_value(), 1 / 70)
+
     def test_vitamin_e_treatment(self):
         """Numerical example from Section 1.3, "Testing a Hypothesis", in
         Chapter 1 of
