d4033ad06ee3d944217f572f67fa8cad40214e23
Artem Mavrin <amavrin@ucsd.edu>
2018-03-06 18:17:02 -0800

    Added 'marker_dict' parameter to 'pca_label_plot'

--- mltools/visualization/classification.py
+++ mltools/visualization/classification.py
@@ -8,7 +8,7 @@ import numpy as np
 from ..preprocessing import PCA
 
 
-def pca_label_plot(x, y_true, y_pred, ax=None, pca=None):
+def pca_label_plot(x, y_true, y_pred, ax=None, pca=None, marker_dict=None):
     """Reduce data to 2 dimensions and plot predicted labels colored according
     to whether they were correctly classified.
 
@@ -25,6 +25,8 @@ def pca_label_plot(x, y_true, y_pred, ax=None, pca=None):
     pca: PCA, optional
         The PCA object used to reduce the data. If not specified, a new PCA
         object will be fitted on the provided data.
+    marker_dict: dict, optional
+        Dictionary of how to transform labels into markers for the scatter plot.
 
     Returns
     -------
@@ -44,14 +46,21 @@ def pca_label_plot(x, y_true, y_pred, ax=None, pca=None):
 
     x = pca.transform(x, dim=2)
 
+    if marker_dict is None:
+        marker_dict = dict()
+
     labels = np.unique(np.concatenate((y_true, y_pred)))
     for label_true, label_pred in itertools.product(labels, labels):
-        marker = "$" + str(label_pred) + "$"
-        color = "g" if label_pred == label_true else "r"
+        if label_pred in marker_dict:
+            marker = marker_dict[label_pred]
+        else:
+            marker = "$" + str(label_pred) + "$"
+        color = "green" if label_pred == label_true else "red"
+        zorder = 1 if label_pred == label_true else 2
         idx = (y_true == label_true) & (y_pred == label_pred)
-        ax.scatter(x[idx, 0], x[idx, 1], c=color, marker=marker)
+        ax.scatter(x[idx, 0], x[idx, 1], c=color, marker=marker, zorder=zorder)
 
-    ax.set_xlabel("1st principal axis")
-    ax.set_ylabel("2nd principal axis")
+    ax.set_xlabel("1st Principal Axis")
+    ax.set_ylabel("2nd Principal Axis")
 
     return ax, pca
