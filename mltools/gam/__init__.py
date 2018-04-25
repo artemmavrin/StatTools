8ed426469551c3dd6ab336c9e5030cf75f9cae4f
Artem Mavrin <amavrin@ucsd.edu>
2018-04-25 09:58:37 -0700

    Implemented k nearest neighbors smoothing

--- mltools/gam/__init__.py
+++ mltools/gam/__init__.py
@@ -1,5 +1,6 @@
 """Generalized additive models."""
 
 from .bin import BinSmoother
+from .knn import KNNSmoother
 from .smoothing import BaggingSmoother
 from .polynomial import PolynomialSmoother
---
+++ mltools/gam/knn.py
@@ -0,0 +1,69 @@
+"""k nearest neighbors smoothers."""
+
+import numbers
+
+import numpy as np
+
+from .smoothing import ScatterplotSmoother
+from ..utils.validation import validate_samples
+
+
+class KNNSmoother(ScatterplotSmoother):
+    """Naive k nearest neighbors scatterplot smoother implementation."""
+
+    # Training predictors
+    x: np.ndarray = None
+
+    # Trainijng response
+    y: np.ndarray = None
+
+    def __init__(self, k=10):
+        """Initialize the KNNSmoother.
+
+        Parameters
+        ----------
+        k : int
+            Number of neighbors.
+        """
+        if not isinstance(k, numbers.Integral) or k <= 0:
+            raise ValueError("Parameter 'k' must be a positive integer.")
+        self.k = int(k)
+
+    def fit(self, x, y):
+        """Store the training data.
+
+        Parameters
+        ----------
+        x : array-like, shape (n,)
+            Explanatory variable.
+        y : array-like, shape (n,)
+            Response variable.
+
+        Returns
+        -------
+        This KNNSmoother instance.
+        """
+        self.x, self.y = validate_samples(x, y, n_dim=1, equal_lengths=True)
+        if self.k > len(x):
+            raise ValueError("Sample size too small.")
+        return self
+
+    def predict(self, x):
+        """Compute averages of training points closest to the points in x.
+
+        Parameters
+        ----------
+        x : array-like, shape (n, )
+            Explanatory variable.
+
+        Returns
+        -------
+        The kNN smoother prediction.
+        """
+        x = validate_samples(x, n_dim=1)
+        y = np.empty(len(x))
+
+        for i, x0 in enumerate(x):
+            idx = np.abs(self.x - x0).argsort()
+            y[i] = self.y[idx[:self.k]].mean()
+        return y
