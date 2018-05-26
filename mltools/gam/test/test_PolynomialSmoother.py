aa185aa23c4dedcaff511dd2f489d31ffdd1e6ab
Artem Mavrin <amavrin@ucsd.edu>
2018-05-26 16:08:18 -0700

    Moved PolynomialRegression -> PolynomialSmoother unit test

--- mltools/gam/test/test_PolynomialSmoother.py
+++ mltools/gam/test/test_PolynomialSmoother.py
@@ -1,14 +1,14 @@
-"""Unit tests for the PolynomialRegression class."""
+"""Unit tests for the PolynomialSmoother class."""
 
 import unittest
 from itertools import product
 
 import numpy as np
 
-from mltools.glm import PolynomialRegression
+from mltools.gam import PolynomialSmoother
 
 
-class TestPolynomialRegression(unittest.TestCase):
+class TestPolynomialSmoother(unittest.TestCase):
     def test_perfect_polynomial_interpolation(self):
         rs = np.random.RandomState(0)
         for deg, _ in product(range(1, 10), range(5)):
@@ -17,13 +17,13 @@ class TestPolynomialRegression(unittest.TestCase):
             x = rs.uniform(low=-4, high=4, size=10)
             y = poly(x)
 
-            model = PolynomialRegression(deg=deg, standardize=False)
+            model = PolynomialSmoother(deg=deg, standardize=False)
             model.fit(x, y)
             np.testing.assert_almost_equal(model.predict(x), y)
             np.testing.assert_almost_equal(model.intercept, coefficients[0])
             np.testing.assert_almost_equal(model.coef, coefficients[1:])
 
-            model = PolynomialRegression(deg=deg, standardize=True)
+            model = PolynomialSmoother(deg=deg, standardize=True)
             model.fit(x, y)
             np.testing.assert_almost_equal(model.predict(x), y)
             np.testing.assert_almost_equal(model.intercept, coefficients[0])
