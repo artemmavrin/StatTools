aa303fdd8c78ef26a9423095b00a5ee481176c5e
Artem Mavrin <amavrin@ucsd.edu>
2018-05-26 17:27:56 -0700

    Removed summary function; added BaseSummary class

--- mltools/utils/__init__.py
+++ mltools/utils/__init__.py
@@ -1,5 +1,5 @@
 """Miscellaneous utility functions and classes for MLTools."""
 
-from .summary import summary
+from .summary import BaseSummary
 from .validation import validate_samples
 from .validation import validate_func
