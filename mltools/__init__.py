3e8d8a9576868096c24b35ad6f062a9377403188
Artem Mavrin <amavrin@ucsd.edu>
2018-05-31 14:27:58 -0700

    Updated mltools/__init__.py

--- mltools/__init__.py
+++ mltools/__init__.py
@@ -1,1 +1,14 @@
 """Machine learning algorithms implemented in Python."""
+
+import mltools.datasets
+import mltools.ensemble
+import mltools.gam
+import mltools.generic
+import mltools.glm
+import mltools.optimization
+import mltools.preprocessing
+import mltools.regularization
+import mltools.resampling
+import mltools.sde
+import mltools.utils
+import mltools.visualization
